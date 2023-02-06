7f45100b88aa2b8de51d4a429dfee68dd24cc061
VJBots <122880036+VJBots@users.noreply.github.com>
2023-02-06 09:01:27 +0530

    Update env.py

--- env.py
+++ env.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = os.getenv("API_ID", "").strip()
-API_HASH = os.getenv("API_HASH", "").strip()
-BOT_TOKEN = os.getenv("BOT_TOKEN", "").strip()
-DATABASE_URL = os.getenv("DATABASE_URL", "").strip()
-MUST_JOIN = os.getenv("MUST_JOIN", "")
+API_ID = os.getenv("API_ID", "27639102").strip()
+API_HASH = os.getenv("API_HASH", "35142c1407be6264e68fb6bec5dcabd9").strip()
+BOT_TOKEN = os.getenv("BOT_TOKEN", "6191852838:AAG6RpgZO0EV_zrivbUO9j31h3N4_bZRTgg").strip()
+DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://Demo123:Demo123@cluster0.07fqvhs.mongodb.net/?retryWrites=true&w=majority").strip()
+MUST_JOIN = os.getenv("MUST_JOIN", "https://t.me/VJ_Bots")
 
 if not API_ID:
     print("No API_ID found. Exiting...")
