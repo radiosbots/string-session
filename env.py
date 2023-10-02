4da8da43b49c485e3cdf539015537505c434a0e9
radiosbots <126034900+radiosbots@users.noreply.github.com>
2023-10-02 10:20:02 +0200

    Update env.py

--- env.py
+++ env.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = os.getenv("API_ID", "27639102").strip()
-API_HASH = os.getenv("API_HASH", "35142c1407be6264e68fb6bec5dcabd9").strip()
-BOT_TOKEN = os.getenv("BOT_TOKEN", "6447675318:AAENIoKK2Rb0RRXwiP8UXd8pswJA1prvbQI").strip()
-DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://Forward123:Forward123@cluster0.4d1ljfv.mongodb.net/?retryWrites=true&w=majority")
-MUST_JOIN = os.getenv("MUST_JOIN", "https://t.me/VJ_Bots")
+API_ID = os.getenv("API_ID", "15377279").strip()
+API_HASH = os.getenv("API_HASH", "9b4f9729873e22a7a30a121edc6c1f70").strip()
+BOT_TOKEN = os.getenv("BOT_TOKEN", "6339227402:AAFXFuSFMX-m-StrFer4v_B1fWe18m0Z0iI").strip()
+DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://alrufaaey:engmomo-1@cluster0.9zpctum.mongodb.net/?retryWrites=true&w=majority")
+MUST_JOIN = os.getenv("MUST_JOIN", "https://t.me/BBeBB")
 
 if not API_ID:
     print("No API_ID found. Exiting...")
